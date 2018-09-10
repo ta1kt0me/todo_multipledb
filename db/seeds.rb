58444b78acaec3fab9c18f2cf4014e9a48d663a8
ta1kt0me <p.wadachi@gmail.com>
2018-09-10 11:54:54 +0900

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Insect.create(name: "butterfly")
+Animal.create(name: "cat")
